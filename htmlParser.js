0d50e07b19a7dd0063e233447b4c19d99bb05667
Li Ge <lge@ucsd.edu>
2015-02-23 05:21:02 -0800

    parse options, add error handle for html parsing

--- htmlParser.js
+++ htmlParser.js
@@ -4,24 +4,29 @@ var _ = require('highland');
 // Input stream of html string
 // Output a single window object on success.
 // TODO: error handling
+// TODO: handle gziped stream
 function htmlParser(s) {
   return s.collect()
     .map(function(strs) { return strs.join(''); })
     .consume(function(err, htmlStr, push, next) {
-      jsdom.env({
-        html: htmlStr,
-        done: function(errors, window) {
-          if (errors) {
-            push(errors);
-            next();
-          } else if (htmlStr === _.nil) {
-            push(null, _.nil);
-          } else {
-            push(null, window);
-            next();
+      try {
+        jsdom.env({
+          html: htmlStr,
+          done: function(errors, window) {
+            if (errors) {
+              push(errors);
+              next();
+            } else if (htmlStr === _.nil) {
+              push(null, _.nil);
+            } else {
+              push(null, window);
+              next();
+            }
           }
-        }
-      });
+        });
+      } catch(err) {
+        push(null, _.nil);
+      }
     });
 }
 
